dbec2bd3e1bb7e1a4c6cc5fa3ff85756a0e2f0d1
jeian.nueva <jeian.nueva@gmail.com>
2017-03-22 09:20:38 -0400

    nitial comi

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Sphinx</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Sphinx Development</div>
+                  <p style="font-size:24;">
+                    Members:
+                  </p>
+          </div>
+          <div>
+                  <ul style="list-style: none;">
+                  <li>Joem Casusi</li>
+                  <li>Jeian Nueva</li>
+                  <li>Vincent Paulin</li>
+                  <li>Jan Gabriel Zobel Tan</li>
+              </ul>
+            </div>
+        </div>
+    </body>
+</html>
